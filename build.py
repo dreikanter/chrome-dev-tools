000efa61489ee37b8f41df1e7b7f920d194530b1
Alex Musayev <alex.musayev@gmail.com>
2012-06-20 14:34:57 +0400

    Zip name generation updated.

--- build.py
+++ build.py
@@ -23,12 +23,16 @@ def get_manifest_details():
 		manifest = json.load(f)
 		return manifest["name"], manifest["version"]
 
+def get_zip_name(name, version):
+	"""Generates command line-friendly archive name from extension name and version."""
+	zip_name = ZIP_NAME.format(name=name, version=version).strip().lower()
+	return re.sub(r"[^a-zA-Z\d_\-\.]+", "", re.sub(r"\s+", "-", zip_name))
+
 def get_cmd():
 	name, version = get_manifest_details()
 	print("Extension name: %s\nVersion: %s" % (name, version))
-	zip_name = ZIP_NAME.format(name=name.lower(), version=version)
 	return ZIP_COMMAND.format(source=os.path.join(SRC_PATH, '*'),
-		dest=os.path.join(BUILD_PATH, zip_name))
+		dest=os.path.join(BUILD_PATH, get_zip_name(name, version)))
 
 
 if(not os.path.exists(BUILD_PATH)):
